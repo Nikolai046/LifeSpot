42bdb2300c63f276b117041cb77569042afffd18
Nick A <nakursk@gmail.com>
2024-12-16 08:12:09 +0300

    JS_2

---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,6 @@
+<script>
+    window.addEventListener('load', function() {
+        let userName = prompt("Пожалуйста, введите ваше имя");
+    alert(`Приветствуем, ${userName}. В вашем имени ${userName.length} символов`);
+    });
+</script>
