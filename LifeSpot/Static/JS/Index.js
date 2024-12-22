fc55a52be5f9585a97c78461518fd4f15f346e55
Nick A <nakursk@gmail.com>
2024-12-22 21:59:11 +0300

    JS_6

--- LifeSpot/Static/JS/Index.js
+++ LifeSpot/Static/JS/Index.js
@@ -1,9 +1,12 @@
 
-function getAccess() {
-    let age;
-    let userData = new Map();
-    userData.set("sessionDate", new Date().toLocaleString());
-    userData.set("UserAgent", window.navigator.userAgent);
+let age;
+let userData = new Map();
+checkAge();
+handleSession();
+sessionLog();
+
+setTimeout(() => alert("Нравится LifeSpot? " + '\n' + "Подпишитесь на наш Instagram @lifespot999!"), 30000);
+function checkAge() {
     while (true) {
         age = prompt("Пожалуйста введите ваш возраст");
         age = parseInt(age); // Преобразуем введённое значение в число
@@ -21,19 +24,33 @@ function getAccess() {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет!");
         window.location.replace("https://google.com");
     }
+}
+
+function handleSession() {
+    userData.set("userAge", age);
+    userData.set("sessionDate", new Date().toLocaleString());
+    userData.set("UserAgent", window.navigator.userAgent);
+}
+
+function sessionLog() {
     for (let [key, value] of userData) {
         console.log(key + ": " + value);
     }
 }
 
-function filterContent() {
-    document.getElementById("myInput").addEventListener("input", function () {
-        const searchText = this.value.toLowerCase();
 
-        const elements = document.querySelectorAll('.video-container > div');
-        for (let i = 0; i < elements.length; i++) {
-            const videoDescription = elements[i].getElementsByTagName('p')[0].innerText.toLowerCase();
-            elements[i].style.display = videoDescription.includes(searchText) ? 'block' : 'none';
-        }
-    });
+//const inputData = function () {
+//    return document.getElementsByTagName('input')[0].value;
+//}
+const inputData = function () {
+return document.getElementById("myInput").value;
+}
+
+function filterContent() {
+    const elements = document.querySelectorAll('.video-container > div');
+    for (let i = 0; i < elements.length; i++) {
+        const videoDescription = elements[i].getElementsByTagName('p')[0].innerText.toLowerCase();
+        elements[i].style.display = videoDescription.includes(inputData()) ? 'block' : 'none';
+    }
+
 }
